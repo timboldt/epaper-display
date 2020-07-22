8212e91150472e7b09ebc02e2d8699f5522b9e4a
Tim Boldt <tim.boldt@gmail.com>
2020-07-21 23:44:41 -0700

    Add RTC

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"fmt"
 	"machine"
 
 	"image/color"
 	"time"
 
+	"tinygo.org/x/drivers/ds3231"
 	"tinygo.org/x/drivers/ssd1306"
 	"tinygo.org/x/tinyfont"
+	"tinygo.org/x/tinyfont/freesans"
 	"tinygo.org/x/tinyfont/proggy"
 )
 
 func main() {
+	time.Sleep(3 * time.Second)
+	println("before")
+
 	machine.I2C0.Configure(machine.I2CConfig{
 		Frequency: machine.TWI_FREQ_400KHZ,
 	})
+
+	clock := ds3231.New(machine.I2C0)
+	clock.Configure()
+	// t, _ := time.Parse(time.RFC3339, "2020-07-21T19:44:00-07:00")
+	// clock.SetTime(t)
+	println("after clock")
 
 	display := ssd1306.NewI2C(machine.I2C0)
 	display.Configure(ssd1306.Config{
@@ -22,38 +34,21 @@ func main() {
 		Width:   128,
 		Height:  32,
 	})
-
 	display.ClearDisplay()
-
 	tinyfont.WriteLine(&display, &proggy.TinySZ8pt7b, 5, 20, []byte("Hello World"), color.RGBA{255, 255, 255, 255})
-	//tinyfont.WriteLine(&display, &freeserif.Regular12pt7b, 5, 20, []byte("Hello World"), color.RGBA{255, 255, 255, 255})
-	//tinyfont.WriteLine(&display, &freemono.Regular9pt7b, 5, 20, []byte("Hello World"), color.RGBA{255, 255, 255, 255})
 	display.Display()
-	time.Sleep(20 * time.Second)
+	println("after display")
 
-	x := int16(0)
-	y := int16(0)
-	deltaX := int16(1)
-	deltaY := int16(1)
 	for {
-		pixel := display.GetPixel(x, y)
-		c := color.RGBA{255, 255, 255, 255}
-		if pixel {
-			c = color.RGBA{0, 0, 0, 255}
-		}
-		display.SetPixel(x, y, c)
+		temp, _ := clock.ReadTemperature()
+		tim, _ := clock.ReadTime()
+		println(temp, tim.Format(time.RFC3339))
+
+		display.ClearDisplay()
+		tinyfont.WriteLine(&display, &proggy.TinySZ8pt7b, 0, 6, []byte(fmt.Sprintf("Temp: %.2f C", float64(temp)/1000)), color.RGBA{255, 255, 255, 255})
+		tinyfont.WriteLine(&display, &freesans.Bold12pt7b, 0, 25, []byte(fmt.Sprintf("%s", tim.Format(time.Kitchen))), color.RGBA{255, 255, 255, 255})
 		display.Display()
 
-		x += deltaX
-		y += deltaY
-
-		if x == 0 || x == 127 {
-			deltaX = -deltaX
-		}
-
-		if y == 0 || y == 31 {
-			deltaY = -deltaY
-		}
-		time.Sleep(1 * time.Millisecond)
+		time.Sleep(60 * time.Second)
 	}
 }
