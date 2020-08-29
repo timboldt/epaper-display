1cd3dd1efd2ce66741504880d8ce532fa9bdbfa4
Tim Boldt <tim.boldt@gmail.com>
2020-08-28 19:48:01 -0700

    Add primitive support for RTC

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,6 +1,7 @@
 #![no_std]
 #![no_main]
 
+extern crate ds323x;
 extern crate embedded_graphics;
 extern crate feather_m0 as hal;
 extern crate panic_halt;
@@ -14,6 +15,7 @@ use hal::pac::{CorePeripherals, Peripherals};
 use hal::prelude::*;
 use hal::time::KiloHertz;
 
+use ds323x::{Ds323x, NaiveDateTime, Rtcc};
 use embedded_graphics::pixelcolor::BinaryColor;
 use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::{Circle, Rectangle, Triangle};
@@ -49,6 +51,11 @@ fn main() -> ! {
 
     let i2c_bus = shared_bus::BusManagerSimple::new(i2c);
 
+    let mut rtc = Ds323x::new_ds3231(i2c_bus.acquire_i2c());
+    let _clk_temp = rtc.get_temperature().unwrap();
+    let _clk_time = rtc.get_datetime().unwrap();
+    //println!("Time: {}", time);
+
     let mut disp: GraphicsMode<_> = Builder::new()
         .size(DisplaySize::Display128x32)
         .connect_i2c(i2c_bus.acquire_i2c())
@@ -67,10 +74,12 @@ fn main() -> ! {
     let y_max = 31;
 
     // screen outline
+    if rtc.get_seconds().unwrap() > 30 {
     Rectangle::new(Point::new(0, 0), Point::new(x_max, y_max))
         .into_styled(style)
         .draw(&mut disp)
         .unwrap();
+    }
 
     // triangle
     // 'Triangle' requires 'embedded_graphics' 0.6 or newer...
