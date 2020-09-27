323e978dba92537271e4c1b31e5c1e11fed6d8b8
Tim Boldt <tim.boldt@gmail.com>
2020-09-26 22:07:21 -0700

    Added BME support

--- rpi-go/go.mod
+++ rpi-go/go.mod
@@ -5,4 +5,5 @@ go 1.14
 require (
 	github.com/Finnhub-Stock-API/finnhub-go v1.2.1
 	github.com/briandowns/openweathermap v0.16.0
+	periph.io/x/periph v3.6.4+incompatible
 )
--- rpi-go/main.go
+++ rpi-go/main.go
@@ -8,9 +8,47 @@ import (
 
 	finnhub "github.com/Finnhub-Stock-API/finnhub-go"
 	owm "github.com/briandowns/openweathermap"
+	"periph.io/x/periph/conn/i2c/i2creg"
+	"periph.io/x/periph/conn/physic"
+	"periph.io/x/periph/devices/bmxx80"
+	"periph.io/x/periph/host"
 )
 
 func main() {
+	//
+	// BME280
+	//
+
+	// Load all the drivers:
+	fmt.Println("host.Init")
+	if _, err := host.Init(); err != nil {
+		log.Fatal(err)
+	}
+
+	// Open a handle to the first available I²C bus:
+	fmt.Println("i2creg.open")
+	bus, err := i2creg.Open("")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer bus.Close()
+
+	// Open a handle to a bme280/bmp280 connected on the I²C bus using default
+	// settings:
+	fmt.Println("bmxx80.NewI2C")
+	dev, err := bmxx80.NewI2C(bus, 0x76, &bmxx80.DefaultOpts)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer dev.Halt()
+
+	// Read temperature from the sensor:
+	var env physic.Env
+	if err = dev.Sense(&env); err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("%8s %10s %9s\n", env.Temperature, env.Pressure, env.Humidity)
+
 	//
 	// FinnHub
 	//
