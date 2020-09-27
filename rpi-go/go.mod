218d2c0e9afa7532978b0c5cf0b74c836f60347e
Tim Boldt <tim.boldt@gmail.com>
2020-09-26 21:44:16 -0700

    Added FinnHub support

--- rpi-go/go.mod
+++ rpi-go/go.mod
@@ -1,3 +1,5 @@
 module github.com/timboldt/epaper-display/eclock
 
 go 1.14
+
+require github.com/Finnhub-Stock-API/finnhub-go v1.2.1
--- rpi-go/main.go
+++ rpi-go/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+
+	finnhub "github.com/Finnhub-Stock-API/finnhub-go"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
+	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
+		Key: os.Getenv("FINHUB_API_KEY"),
+	})
+	quote, _, err := finnhubClient.Quote(auth, "GOOG")
+	if err != nil {
+		fmt.Printf("err retrieving quote: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", quote)
+	stockCandles, _, err := finnhubClient.StockCandles(auth, "GOOG", "D", 1590988249, 1591852249, nil)
+	if err != nil {
+		fmt.Printf("err retrieving candles: %v\n", err)
+		os.Exit(1)
+	}
+	fmt.Printf("%+v\n", stockCandles)
 }
