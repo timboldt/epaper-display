4a355b9b7f90f6a2e6a32aa87d39b8f06970d634
Tim Boldt <tim.boldt@gmail.com>
2020-09-26 21:49:52 -0700

    Add OpenWeatherMap data

--- rpi-go/go.mod
+++ rpi-go/go.mod
@@ -2,4 +2,7 @@ module github.com/timboldt/epaper-display/eclock
 
 go 1.14
 
-require github.com/Finnhub-Stock-API/finnhub-go v1.2.1
+require (
+	github.com/Finnhub-Stock-API/finnhub-go v1.2.1
+	github.com/briandowns/openweathermap v0.16.0
+)
--- rpi-go/main.go
+++ rpi-go/main.go
@@ -3,26 +3,39 @@ package main
 import (
 	"context"
 	"fmt"
+	"log"
 	"os"
 
 	finnhub "github.com/Finnhub-Stock-API/finnhub-go"
+	owm "github.com/briandowns/openweathermap"
 )
 
 func main() {
+	//
+	// FinnHub
+	//
 	finnhubClient := finnhub.NewAPIClient(finnhub.NewConfiguration()).DefaultApi
 	auth := context.WithValue(context.Background(), finnhub.ContextAPIKey, finnhub.APIKey{
 		Key: os.Getenv("FINHUB_API_KEY"),
 	})
 	quote, _, err := finnhubClient.Quote(auth, "GOOG")
 	if err != nil {
-		fmt.Printf("err retrieving quote: %v\n", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 	fmt.Printf("%+v\n", quote)
 	stockCandles, _, err := finnhubClient.StockCandles(auth, "GOOG", "D", 1590988249, 1591852249, nil)
 	if err != nil {
-		fmt.Printf("err retrieving candles: %v\n", err)
-		os.Exit(1)
+		log.Fatalln(err)
 	}
 	fmt.Printf("%+v\n", stockCandles)
+
+	//
+	// OpenWeatherMap
+	//
+	w, err := owm.NewCurrent("C", "EN", os.Getenv("OPEN_WEATHER_KEY"))
+	if err != nil {
+		log.Fatalln(err)
+	}
+	w.CurrentByID(5383777)
+	fmt.Println(w)
 }
